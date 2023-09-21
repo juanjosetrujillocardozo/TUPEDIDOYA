0a83bae6ab2500189f6c1d6d30ae08ee5161ed32
WilliamUwU <61254776+Wapg2002@users.noreply.github.com>
2023-09-20 21:32:15 -0500

    se aplica css al login

--- src/js/sign-in.js
+++ src/js/sign-in.js
@@ -25,16 +25,21 @@ d.addEventListener('click', async e => {
 
 
 
-    // se hace la petición por AJAX al backend
-    const res = await fetch('http://localhost:3000/auth/login', {
-      method: 'PUT',
-      headers: {
-        'accept': 'application/json; charset=utf-8',
-        'content-type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({ username: $inputUser.value, password: $inputPassword.value }),
-    });
+    try {
+      // se hace la petición por AJAX al backend
+      const res = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+          'accept': 'application/json; charset=utf-8',
+          'content-type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify({ user: $inputUser.value, password: $inputPassword.value }),
+      }).catch(e => console.log(e));
 
-    console.log(await res.json());
+      console.log(await res.json());
+    } catch (err) {
+      if (err instanceof TypeError)
+        console.log(err.stack);
+    }
   }
 });
