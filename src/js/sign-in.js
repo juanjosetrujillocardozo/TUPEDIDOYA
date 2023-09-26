71f3b07bb0cbd5fde268ce1345c5212ad1e3d220
WilliamUwU <61254776+Wapg2002@users.noreply.github.com>
2023-09-26 00:43:45 -0500

    se completa la integración del login y front

--- src/js/sign-in.js
+++ src/js/sign-in.js
@@ -1,14 +1,40 @@
+// DECLARACIÓN DE VARIABLES
 const d = document;
 
 let $inputUser,
   $inputPassword,
-  $btnSignIn;
+  $btnSignIn,
+  $liErrors;
+
+// DECLARACIÓN DE FUNCIONES
+
+
+const validarErrores = function (errores) {
+  const tpErrors = {};
+  $liErrors.innerHTML = '';
+  if (errores.length) {
+    const $fragment = d.createDocumentFragment();
+    errores.forEach(e => {
+      tpErrors[e.tp] = true;
+      const $li = d.createElement("li");
+      $li.textContent = e.error;
+      $fragment.appendChild($li);
+    });
+    $liErrors.appendChild($fragment);
+    (1 in tpErrors || 3 in tpErrors) ? $inputUser.classList.add('error') : $inputUser.classList.remove('error');
+    (2 in tpErrors || 3 in tpErrors) ? $inputPassword.classList.add('error') : $inputPassword.classList.remove('error');
+  } else {
+    $inputPassword.classList.remove('error');
+    $inputUser.classList.remove('error');
+  }
+}
+
 
 d.addEventListener('DOMContentLoaded', e => {
   $inputUser = d.getElementById('user'),
     $inputPassword = d.getElementById('password'),
-    $btnSignIn = d.getElementById('sign-in-btn');
-
+    $btnSignIn = d.getElementById('sign-in-btn'),
+    $liErrors = d.getElementById('errors');
 });
 
 d.addEventListener('submit', e => {
@@ -18,12 +44,28 @@ d.addEventListener('submit', e => {
 
 d.addEventListener('click', async e => {
   if (e.target.matches('#sign-in-btn')) {
-    // validar el usuario y contraseña
-    if (!$inputUser.value.length) {
-      return alert('falta usuario');
-    }
+    const errors = [];
+    // VALIDACIONES DE USUARIO
+    // quitar espacios en blanco del inicio y del final de la cadena
+    $inputUser.value = $inputUser.value.trim();
 
+    if (!$inputUser.value.length)
+      errors.push({ error: 'falta el usuario', tp: 1 });
 
+    if (!(/^[a-zA-Z0-9@_-]+$/.test($inputUser.value)))
+      errors.push({ error: 'el usuario ingresado no es válido', tp: 1 });
+
+    // VALIDACIONES DE LA CONTRASEÑA
+    $inputPassword.value = $inputPassword.value.trim();
+
+    if (!$inputPassword.value.length)
+      errors.push({ error: 'falta la contraseña', tp: 2 });
+
+    if (!(/^[a-zA-Z0-9@_-]+$/.test($inputPassword.value)))
+      errors.push({ error: 'la contraseña ingresada no es válida', tp: 2 });
+
+    validarErrores(errors);
+    if (errors.length) return;
 
     try {
       // se hace la petición por AJAX al backend
@@ -34,12 +76,24 @@ d.addEventListener('click', async e => {
           'content-type': 'application/json; charset=utf-8',
         },
         body: JSON.stringify({ user: $inputUser.value, password: $inputPassword.value }),
-      }).catch(e => console.log(e));
+      });
 
-      console.log(await res.json());
+
+      if (res.ok) {
+        const response = await res.json();
+        localStorage.setItem('JWT', response.data.msg.access_token);
+        location.href = "http://127.0.0.1:5500/src/html/index.html";
+      } else {
+        if (res.status == 401) {
+          // código http para las credenciales inválidas
+          validarErrores([{ error: 'usuario ó contraseña incorrectos', tp: 3, }]);
+        } else if (res.status == 400) {
+          validarErrores([{ error: 'Los datos se han enviado de forma incorrecta. Por favor recargue la página web e inténtelo nuevamente', tp: 3, }]);
+        }
+      }
     } catch (err) {
       if (err instanceof TypeError)
-        console.log(err.stack);
+        console.error('Ha ocurrido un error al realizar el login.');
     }
   }
 });
