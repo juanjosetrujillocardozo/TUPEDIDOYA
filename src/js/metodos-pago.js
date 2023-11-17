5c92ef0bd883c715786a778b543e7841e07948e4
WilliamUwU <61254776+Wapg2002@users.noreply.github.com>
2023-11-17 01:46:46 -0500

    se crean los cruds de métodos de pago y zonas

---
+++ src/js/metodos-pago.js
@@ -0,0 +1,297 @@
+import { API_URL } from './../constants/constants.js';
+import { appendAlert, fetchRequest, showDeleteConfirmationAlert } from './modules/index.js';
+
+const d = document;
+
+let $nombreMetodoPago,
+  $estado,
+  $liErrors,
+  $nombreMetodoPagoModal,
+  $estadoMetodoPagoModal,
+  $liErrorsMetodoPagoModal,
+  $idMetodoPagoModal,
+  myModal,
+  dataTable;
+
+// DECLARACIÓN DE FUNCIONES
+
+
+const validarErrores = function (serverError = null, editar = false, limpiar = false) {
+  const errores = (serverError) ? serverError : [];
+
+  const $inputNombre = editar ? $nombreMetodoPagoModal : $nombreMetodoPago;
+
+  const $listErrors = editar ? $liErrorsMetodoPagoModal : $liErrors;
+
+
+  if (!limpiar) {
+    $inputNombre.value = $inputNombre.value.trim();
+
+    if (!$inputNombre.value)
+      errores.push({ tp: 1, error: 'Falta el nombre del método de pago' });
+    else if (!(/^[a-zA-ZÁáÉéÍíÓóÚúÜüÑñ0-9\s]+$/.test($inputNombre.value)))
+      errores.push({ tp: 1, error: 'El nombre del método de pago no es válido. Sólo se aceptan letras y números', });
+
+  }
+
+  const tpErrors = {};
+  console.log($listErrors);
+  $listErrors.innerHTML = '';
+  if (errores.length) {
+    const $fragment = d.createDocumentFragment();
+    errores.forEach(e => {
+      tpErrors[e.tp] = true;
+      const $li = d.createElement("li");
+      $li.textContent = e.error;
+      $fragment.appendChild($li);
+    });
+
+    $listErrors.appendChild($fragment);
+    (1 in tpErrors) ? $inputNombre.classList.add('error') : $inputNombre.classList.remove('error');
+  } else {
+    $inputNombre.classList.remove('error');
+  }
+
+  return errores.length;
+}
+
+const cargarMetodosPago = async () => {
+  try {
+
+    const errorCatchMetodosPago = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.log('Ha ocurrido un error al obtener los métodos de pago');
+    };
+
+    let response = await fetchRequest(null, errorCatchMetodosPago, `${API_URL}/payment-method/all`);
+
+    console.log(response);
+    if (!response) response = { data: [] };
+
+    if (dataTable !== undefined)
+      dataTable.destroy();
+
+    dataTable = $('#payment-methods').DataTable({
+      language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+          "first": "Primero",
+          "last": "Ultimo",
+          "next": "Siguiente",
+          "previous": "Anterior"
+        }
+      },
+      data: response.data,
+      "columns": [
+        { data: 'name', },
+        {
+          title: 'Estado',
+          data: 'status',
+          render: function (data, type, row) {
+            const txt = data ? 'Activo' : 'Inactivo';
+            if (type === 'display') {
+              return txt
+            }
+            return txt;
+          }
+        },
+        {
+          title: 'Acciones',
+          orderable: false, // No permite ordenar esta columna
+          data: 'id',
+          render: function (data, type, row) {
+            if (type === 'display') {
+              return `
+                  <button type="button" id="btn-edit-method-payment" data-id-method-payment="${data}" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editPaymentMethodsModal">
+                    <i class="bi bi-pencil-fill"></i>
+                  </button>
+                  <button type="button" id="btn-delete-payment-method" data-id-method-payment="${data}" class="btn btn-secondary btn-sm">
+                    <i class="bi bi-trash-fill"></i>
+                  </button>
+                `
+            }
+            return data;
+          }
+        }
+      ],
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+
+}; //ok
+
+
+// DELEGACIÓN DE EVENTOS
+d.addEventListener('DOMContentLoaded', async e => {
+  $nombreMetodoPago = d.getElementById('name');
+  $estado = d.getElementById('status');
+  $liErrors = d.getElementById('errors');
+
+
+  // INPUTS DE LA MODAL
+  $nombreMetodoPagoModal = d.querySelector('#editPaymentMethodsModal #name');
+  $estadoMetodoPagoModal = d.querySelector('#editPaymentMethodsModal #status');
+  $idMetodoPagoModal = d.querySelector('#editPaymentMethodsModal #id-method-payment');
+  $liErrorsMetodoPagoModal = d.querySelector('#editPaymentMethodsModal #errors');
+
+
+  myModal = new bootstrap.Modal('#editPaymentMethodsModal', {
+    keyboard: false
+  });
+
+  d.getElementById('editPaymentMethodsModal').addEventListener('hide.bs.modal', e => {
+    validarErrores(null, true, true);
+  });
+
+  cargarMetodosPago();
+}); //ok
+
+d.addEventListener('submit', async e => {
+  if (e.target.matches('#form-method-payments')) {
+    e.preventDefault();
+
+    const errors = validarErrores();
+    if (errors) return;
+
+    const onErrorResponse = (res, response) => {
+      console.log(response);
+      if (res.status == 400) {
+        validarErrores([{ error: response.message }]);
+      }
+    };
+
+    const onErrorCatch = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.error('Ha ocurrido un error al crear el método de pago.');
+    };
+
+    // se hace la petición por AJAX al backend
+    const response = await fetchRequest(onErrorResponse, onErrorCatch, `${API_URL}/payment-method/create-payment-method`, 'POST', {
+      name: $nombreMetodoPago.value,
+      status: ($estado[$estado.selectedIndex].value === '1') ? true : false,
+    });
+
+    if (response) {
+      e.target.reset();
+      cargarMetodosPago();
+      appendAlert('Método de pago creado correctamente');
+    }
+
+  }
+});
+
+d.addEventListener('click', async e => {
+
+  if (e.target.matches('#btn-edit-method-payment, #btn-edit-method-payment > *')) {
+
+    const idMethodPayment = (e.target.matches('#btn-edit-method-payment'))
+      ? e.target.getAttribute('data-id-method-payment')
+      : e.target.parentElement.getAttribute('data-id-method-payment');
+
+    console.log(idMethodPayment);
+
+    const onErrorCatch = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.error('Ha ocurrido un error al obtener el método de pago.');
+    };
+
+    // se hace la petición por AJAX al backend
+    const response = await fetchRequest(null, onErrorCatch, `${API_URL}/payment-method/find/${idMethodPayment}`);
+
+    if (response) {
+      console.log(response);
+
+      $nombreMetodoPagoModal.value = response.data.name;
+      $estadoMetodoPagoModal.querySelectorAll('option').forEach(op => {
+        if (op.value == response.data.status)
+          op.setAttribute('selected', 'true');
+        else
+          op.removeAttribute('selected');
+      });
+
+      $idMetodoPagoModal.value = idMethodPayment;
+
+    }
+  }
+
+  if (e.target.matches('#btn-guardar-cambios')) {
+
+    const errors = validarErrores(null, true);
+    if (errors) return;
+
+    const onErrorCatch = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.error('Ha ocurrido un error al editar el método de pago.');
+    };
+
+    // se hace la petición por AJAX al backend
+
+    const response = await fetchRequest(
+      null, onErrorCatch, `${API_URL}/payment-method/update-payment-method/${$idMetodoPagoModal.value}`, 'PATCH',
+      {
+        name: $nombreMetodoPagoModal.value,
+        status: ($estadoMetodoPagoModal[$estadoMetodoPagoModal.selectedIndex].value === '1') ? true : false,
+      }
+    );
+
+    if (response) {
+      cargarMetodosPago();
+      myModal.hide();
+      appendAlert('Método de pago editado correctamente');
+    }
+  }
+
+  if (e.target.matches('#btn-delete-payment-method, #btn-delete-payment-method > *')) {
+
+    const confirmCallback = async (deleteConfirmationAlert) => {
+      const idPaymentMethod = (e.target.matches('#btn-delete-payment-method'))
+        ? e.target.getAttribute('data-id-method-payment')
+        : e.target.parentElement.getAttribute('data-id-method-payment');
+
+      const onErrorCatch = (e) => {
+        console.log(e);
+        if (e instanceof TypeError)
+          console.error('Ha ocurrido un error durante la eliminación del método de pago.');
+      };
+
+      const onErrorResponse = (res, response) => {
+        deleteConfirmationAlert.fire(
+          'Error',
+          (res.status === 422) ? 'No se puede eliminar el método de pago. El método de pago que deseas eliminar ya se encuentra en uso dentro del sistema' : 'No se ha podido eliminar el método de pago.',
+          'error'
+        );
+      }
+
+      const response = await fetchRequest(onErrorResponse, onErrorCatch, `${API_URL}/payment-method/delete-payment-method/${idPaymentMethod}`, 'DELETE');
+
+      if (response) {
+        cargarMetodosPago();
+        deleteConfirmationAlert.fire(
+          'Método de Pago Eliminado',
+          'El método de pago ha sido eliminado satisfactoriamente.',
+          'success'
+        );
+      } else {
+        console.log(response);
+      }
+    };
+    showDeleteConfirmationAlert('Eliminar método de pago', 'Sí eliminas un método de pago, no podrás recuperarlo nuevamente.', confirmCallback);
+  }
+});
---
+++ src/js/zones.js
@@ -0,0 +1,445 @@
+import { API_URL } from './../constants/constants.js';
+import { appendAlert, fetchRequest, showDeleteConfirmationAlert } from './modules/index.js';
+
+const d = document;
+
+let $nombreZona,
+  $departamento,
+  $ciudad,
+  $descripcion,
+  $descuento,
+  $comision,
+  $liErrors,
+  $nombreZonaModal,
+  $departamentoZonaModal,
+  $ciudadZonaModal,
+  $descripcionZonaModal,
+  $descuentoZonaModal,
+  $comisionZonaModal,
+  $liErrorsZonaModal,
+  $idZonaModal,
+  myModal,
+  dataTable;
+
+// DECLARACIÓN DE FUNCIONES
+
+
+const validarErrores = function (serverError = null, editar = false, limpiar = false) {
+  const errores = (serverError) ? serverError : [];
+
+  const $inputNombre = editar ? $nombreZonaModal : $nombreZona;
+  const $inputDepartamento = editar ? $departamentoZonaModal : $departamento;
+  const $inputCiudad = editar ? $ciudadZonaModal : $ciudad;
+  const $inputDescripcion = editar ? $descripcionZonaModal : $descripcion;
+
+  const $listErrors = editar ? $liErrorsZonaModal : $liErrors;
+
+
+  if (!limpiar) {
+    $inputNombre.value = $inputNombre.value.trim();
+    $inputDescripcion.value = $inputDescripcion.value.trim();
+
+    if (!$inputNombre.value)
+      errores.push({ tp: 1, error: 'Falta el nombre de la zona' });
+    else if (!(/^[a-zA-ZÁáÉéÍíÓóÚúÜüÑñ0-9\s]+$/.test($inputNombre.value)))
+      errores.push({ tp: 1, error: 'El nombre de zona no es válido. Sólo se aceptan letras y números', });
+
+    // if (!$inputDepartamento.selectedIndex)
+    //   errores.push({ tp: 2, error: 'Debe seleccionar un departamento', });
+
+    // if (!$inputCiudad.selectedIndex)
+    //   errores.push({ tp: 3, error: 'Debe seleccionar una ciudad', });
+
+    if (!$inputDescripcion.value)
+      errores.push({ tp: 4, error: 'Falta la descripción de la zona' });
+
+  }
+
+  const tpErrors = {};
+  console.log($listErrors);
+  $listErrors.innerHTML = '';
+  if (errores.length) {
+    const $fragment = d.createDocumentFragment();
+    errores.forEach(e => {
+      tpErrors[e.tp] = true;
+      const $li = d.createElement("li");
+      $li.textContent = e.error;
+      $fragment.appendChild($li);
+    });
+
+    $listErrors.appendChild($fragment);
+    (1 in tpErrors) ? $inputNombre.classList.add('error') : $inputNombre.classList.remove('error');
+    (2 in tpErrors) ? $inputDepartamento.classList.add('error') : $inputDepartamento.classList.remove('error');
+    (3 in tpErrors) ? $inputCiudad
+      .classList.add('error') : $inputDepartamento
+        .classList.remove('error');
+    (4 in tpErrors) ? $inputDescripcion.classList.add('error') : $inputDescripcion.classList.remove('error');
+  } else {
+    $inputNombre.classList.remove('error');
+    $inputDepartamento.classList.remove('error');
+    $inputCiudad.classList.remove('error');
+    $inputDescripcion.classList.remove('error');
+  }
+
+  return errores.length;
+}
+
+const cargarZonas = async () => {
+  try {
+
+    const errorCatchZonas = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.log('Ha ocurrido un error al obtener las zonas');
+    };
+
+    let response = await fetchRequest(null, errorCatchZonas, `${API_URL}/zone/all`);
+
+    console.log(response);
+    if (!response) response = { data: [] };
+
+    if (dataTable !== undefined)
+      dataTable.destroy();
+
+    dataTable = $('#zones').DataTable({
+      language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+          "first": "Primero",
+          "last": "Ultimo",
+          "next": "Siguiente",
+          "previous": "Anterior"
+        }
+      },
+      data: response.data,
+      "columns": [
+        { data: 'name', },
+        {
+          title: 'Departamento',
+          data: 'departament_code',
+          render: function (data, type, row) {
+            const departamento = 'pendiente';
+            if (type === 'display') {
+              return departamento
+            }
+            return departamento;
+          }
+        },
+        {
+          title: 'Ciudad',
+          data: 'city_code',
+          render: function (data, type, row) {
+            const ciudad = 'pendiente';
+            if (type === 'display') {
+              return ciudad
+            }
+            return ciudad;
+          }
+        },
+        { data: 'description' },
+        {
+          title: 'Descuento',
+          data: 'discount_id',
+          render: function (data, type, row) {
+            const descuento = (!data) ? 'Sin descuento asignado' : data.name;
+            if (type === 'display') {
+              return descuento
+            }
+            return descuento;
+          }
+        },
+        {
+          title: 'Comisión',
+          data: 'commission_id',
+          render: function (data, type, row) {
+            const comision = (!data) ? 'Sin comisión asignada' : data.name;
+            if (type === 'display') {
+              return comision
+            }
+            return comision;
+          }
+        },
+        {
+          title: 'Acciones',
+          orderable: false, // No permite ordenar esta columna
+          data: 'id',
+          render: function (data, type, row) {
+            if (type === 'display') {
+              return `
+                  <button type="button" id="btn-edit-zone" data-id-zone="${data}" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editZonesModal">
+                    <i class="bi bi-pencil-fill"></i>
+                  </button>
+                  <button type="button" id="btn-delete-zone" data-id-zone="${data}" class="btn btn-secondary btn-sm">
+                    <i class="bi bi-trash-fill"></i>
+                  </button>
+                `
+            }
+            return data;
+          }
+        }
+      ],
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+
+};
+
+const obtenerDescuentos = async (editar = false, idDescuento = null) => {
+  const errorCatchDescuentos = (e) => {
+    console.log(e);
+    if (e instanceof TypeError)
+      console.log('Ha ocurrido un error al obtener los descuentos');
+  };
+
+  let response = await fetchRequest(null, errorCatchDescuentos, `${API_URL}/discount/all`);
+  console.log(response);
+  // response.data = response.data.filter(g => g.status);
+
+  const $selectDescuentos = editar ? $descuentoZonaModal : $descuento;
+
+  if (!response || !response.data.length) {
+    $selectDescuentos.innerHTML = `<option>No hay descuentos</option>`;
+  } else {
+    const $fragment = d.createDocumentFragment();
+    $selectDescuentos.innerHTML = '<option disabled selected>Seleccione...</option>';
+    for (const descuento of response.data) {
+      const $option = d.createElement('option');
+      $option.textContent = descuento.name;
+      $option.setAttribute('value', descuento.id);
+      if (editar && descuento.id === idDescuento)
+        $option.setAttribute('selected', 'true');
+      $fragment.appendChild($option);
+    }
+    $selectDescuentos.appendChild($fragment);
+
+  }
+};
+
+const obtenerComisiones = async (editar = false, idComision = null) => {
+  const errorCatchComisiones = (e) => {
+    console.log(e);
+    if (e instanceof TypeError)
+      console.log('Ha ocurrido un error al obtener las comisiones');
+  };
+
+  let response = await fetchRequest(null, errorCatchComisiones, `${API_URL}/commission/all`);
+  console.log(response);
+  // response.data = response.data.filter(g => g.status);
+
+  const $selectComisiones = editar ? $comisionZonaModal : $comision;
+
+  if (!response || !response.data.length) {
+    $selectComisiones.innerHTML = `<option>No hay comisiones</option>`;
+  } else {
+    const $fragment = d.createDocumentFragment();
+    $selectComisiones.innerHTML = '<option disabled selected>Seleccione...</option>';
+    for (const comision of response.data) {
+      const $option = d.createElement('option');
+      $option.textContent = comision.name;
+      $option.setAttribute('value', comision.id);
+      if (editar && comision.id === idComision)
+        $option.setAttribute('selected', 'true');
+      $fragment.appendChild($option);
+    }
+    $selectComisiones.appendChild($fragment);
+
+  }
+};
+
+
+// DELEGACIÓN DE EVENTOS
+d.addEventListener('DOMContentLoaded', async e => {
+  $nombreZona = d.getElementById('name');
+  $departamento = d.getElementById('departament');
+  $ciudad = d.getElementById('city');
+  $descripcion = d.getElementById('description');
+  $descuento = d.getElementById('discount');
+  $comision = d.getElementById('comission');
+  $liErrors = d.getElementById('errors');
+
+
+  // INPUTS DE LA MODAL
+  $nombreZonaModal = d.querySelector('#editZonesModal #name');
+  $departamentoZonaModal = d.querySelector('#editZonesModal #departament');
+  $ciudadZonaModal = d.querySelector('#editZonesModal #city');
+  $descripcionZonaModal = d.querySelector('#editZonesModal #description');
+  $descuentoZonaModal = d.querySelector('#editZonesModal #discount');
+  $comisionZonaModal = d.querySelector('#editZonesModal #comission');
+  $idZonaModal = d.querySelector('#editZonesModal #id-zones-modal');
+  $liErrorsZonaModal = d.querySelector('#editZonesModal #errors');
+
+  await obtenerDescuentos();
+  await obtenerComisiones();
+
+
+  myModal = new bootstrap.Modal('#editZonesModal', {
+    keyboard: false
+  });
+
+  d.getElementById('editZonesModal').addEventListener('hide.bs.modal', e => {
+    validarErrores(null, true, true);
+  });
+
+  cargarZonas();
+}); //ok
+
+d.addEventListener('submit', async e => {
+  if (e.target.matches('#form-zones')) {
+    e.preventDefault();
+
+    const errors = validarErrores();
+    if (errors) return;
+
+    const onErrorResponse = (res, response) => {
+      console.log(response);
+      if (res.status == 400) {
+        validarErrores([{ error: response.message }]);
+      }
+    };
+
+    const onErrorCatch = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.error('Ha ocurrido un error al crear la zona.');
+    };
+
+    // se hace la petición por AJAX al backend
+    const response = await fetchRequest(onErrorResponse, onErrorCatch, `${API_URL}/zone/create-zone`, 'POST', {
+      name: $nombreZona.value,
+      departament_cod: "0",
+      city_code: "0",
+      description: $descripcion.value,
+      discount: parseInt((!$descuento.selectedIndex) ? null : $descuento[$descuento.selectedIndex].value),
+      commission: parseInt((!$comision.selectedIndex) ? null : $comision[$comision.selectedIndex].value),
+    });
+
+    if (response) {
+      e.target.reset();
+      cargarZonas();
+      appendAlert('Zona creada correctamente');
+    }
+
+  }
+});
+
+d.addEventListener('click', async e => {
+
+  if (e.target.matches('#btn-edit-zone, #btn-edit-zone > *')) {
+
+    const idZone = (e.target.matches('#btn-edit-zone'))
+      ? e.target.getAttribute('data-id-zone')
+      : e.target.parentElement.getAttribute('data-id-zone');
+
+    console.log(idZone);
+
+    const onErrorCatch = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.error('Ha ocurrido un error al obtener la zona.');
+    };
+
+    // se hace la petición por AJAX al backend
+    const response = await fetchRequest(null, onErrorCatch, `${API_URL}/zone/find/${idZone}`);
+
+    if (response) {
+      console.log(response);
+
+      $nombreZonaModal.value = response.data.name;
+      // $ciudadZonaModal.value = response.data;
+      // $departamentoZonaModal.querySelectorAll('option').forEach(op => {
+      //   if (op.value == response.data.type)
+      //     op.setAttribute('selected', 'true');
+      //   else
+      //     op.removeAttribute('selected');
+      // });
+      $descripcionZonaModal.value = response.data.description;
+
+      $idZonaModal.value = idZone;
+      await obtenerDescuentos(true, response.data.discount_id?.id);
+      await obtenerComisiones(true, response.data.commission_id?.id);
+
+    }
+  }
+
+  if (e.target.matches('#btn-guardar-cambios')) {
+
+    const errors = validarErrores(null, true);
+    if (errors) return;
+
+    const onErrorCatch = (e) => {
+      console.log(e);
+      if (e instanceof TypeError)
+        console.error('Ha ocurrido un error al editar la zona.');
+    };
+
+    // se hace la petición por AJAX al backend
+
+    const response = await fetchRequest(
+      null, onErrorCatch, `${API_URL}/zone/update-zone/${$idZonaModal.value}`, 'PATCH',
+      {
+        name: $nombreZonaModal.value,
+        departament_cod: "0",
+        city_code: "0",
+        description: $descripcionZonaModal.value,
+        discount: parseInt((!$descuentoZonaModal.selectedIndex) ? null : $descuentoZonaModal[$descuentoZonaModal.selectedIndex].value),
+        commission: parseInt((!$comisionZonaModal.selectedIndex) ? null : $comisionZonaModal[$comisionZonaModal.selectedIndex].value),
+      }
+    );
+
+    if (response) {
+      cargarZonas();
+      myModal.hide();
+      appendAlert('Zona editada correctamente');
+    }
+  }
+
+  if (e.target.matches('#btn-delete-zone, #btn-delete-zone > *')) {
+
+    const confirmCallback = async (deleteConfirmationAlert) => {
+      const idZona = (e.target.matches('#btn-delete-zone'))
+        ? e.target.getAttribute('data-id-zone')
+        : e.target.parentElement.getAttribute('data-id-zone');
+
+      const onErrorCatch = (e) => {
+        console.log(e);
+        if (e instanceof TypeError)
+          console.error('Ha ocurrido un error durante la eliminación de la zona.');
+      };
+
+      const onErrorResponse = (res, response) => {
+        deleteConfirmationAlert.fire(
+          'Error',
+          (res.status === 422) ? 'No se puede eliminar la zona. La zona que deseas eliminar ya se encuentra en uso dentro del sistema' : 'No se ha podido eliminar la zona.',
+          'error'
+        );
+      }
+
+      const response = await fetchRequest(onErrorResponse, onErrorCatch, `${API_URL}/zone/delete-zone/${idZona}`, 'DELETE');
+
+      if (response) {
+        cargarZonas();
+        deleteConfirmationAlert.fire(
+          'Zona eliminada',
+          'La zona ha sido eliminada satisfactoriamente.',
+          'success'
+        );
+      } else {
+        console.log(response);
+      }
+    };
+    showDeleteConfirmationAlert('Eliminar zona', 'Sí eliminas una zona, no podrás recuperarla nuevamente.', confirmCallback);
+  }
+});
